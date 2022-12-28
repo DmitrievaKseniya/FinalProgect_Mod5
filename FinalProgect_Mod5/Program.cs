9569716fbbf88460aa4004e61e4c6512f49c6b7f
DmitrievaKseniya <dkn1933@mail.ru>
2022-12-28 22:42:35 +0400

    Добавьте файлы проекта.

---
+++ FinalProgect_Mod5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalProgect_Mod5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
